b643406a7467134db340b443a73483c9c415ad49
osama mohamoud <wwwhdosama@gmail.com>
2026-04-06 19:48:07 +0200

    Auto commit: Mon Apr  6 19:48:05 EST 2026

---
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp7/Form2.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp7.Properties.Resources.X;
+            this.pictureBox1.Location = new System.Drawing.Point(201, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(577, 332);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(968, 90);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "لابوبو";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "لابوبو",
+            "boy",
+            "girl",
+            "books"});
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "book",
+            "boy",
+            "girl",
+            "لابوبو"});
+            this.comboBox1.Location = new System.Drawing.Point(319, 453);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(328, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(968, 535);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp7/Form2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp7.Properties;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        void ChangePhoto(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                   
+                  pictureBox1.Image = Resources.PicBooks_;
+                    break;
+
+                case 1:
+                    pictureBox1.Image = Resources._38459;
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Resources.picGril;
+                    break;
+
+                case 3:
+                    pictureBox1.Image = Resources.X;
+                    break;
+
+            }
+
+        }
+
+        void ChangePhoto(int index)
+        {
+            switch (index)
+            {
+                case 0:
+
+                    pictureBox1.Image = Resources.PicBooks_;
+                    break;
+
+                case 1:
+                    pictureBox1.Image = Resources._38459;
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Resources.picGril;
+                    break;
+
+                case 3:
+                    pictureBox1.Image = Resources.X;
+                    break;
+
+            }
+
+        }
+
+        void ChangeLabel(int index )
+        {
+            switch (index)
+            {
+                case 0:
+                    label1.Text = "books";
+                    label1.ForeColor = Color.PowderBlue;
+                    break;
+                   
+
+                case 1:
+                    label1.Text = "Boy";
+                    label1.ForeColor = Color.GreenYellow;
+                    break;
+
+                case 2:
+                    label1.Text = "girl";
+                    label1.ForeColor = Color.HotPink;
+                    break;
+
+                case 3:
+                    label1.Text = "لا بوبو عمك";
+                    label1.ForeColor = Color.Goldenrod;
+                    break;
+
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            ChangePhoto(comboBox.SelectedIndex);
+            ChangeLabel(comboBox.SelectedIndex);
+        }
+    }
+}
