76b82fb6f6e1255debec1747434d243589ffef8d
osama mohamoud <wwwhdosama@gmail.com>
2026-04-10 16:51:05 +0200

    Auto commit: Fri Apr 10 16:51:04 EST 2026

---
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp9/AgeCalc.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class AgeCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labRes = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("vivo Sans Clock Stencil", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(320, 65);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2026, 4, 10, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(576, 46);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.UseWaitCursor = true;
+            this.dateTimePicker1.Value = new System.DateTime(2026, 4, 10, 0, 0, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1187, 62);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Age Calc";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mudir MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Your Birth Date";
+            // 
+            // labRes
+            // 
+            this.labRes.AutoSize = true;
+            this.labRes.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.labRes.Location = new System.Drawing.Point(304, 237);
+            this.labRes.Name = "labRes";
+            this.labRes.Size = new System.Drawing.Size(51, 35);
+            this.labRes.TabIndex = 3;
+            this.labRes.Text = "res";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "Get Your Age in Days"});
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Get Your Age in Days",
+            "Get Your Age in Months",
+            "Get Your Age in Hours"});
+            this.comboBox1.Location = new System.Drawing.Point(336, 375);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(457, 33);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.UseWaitCursor = true;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // AgeCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 550);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labRes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "AgeCalc";
+            this.Text = "AgeCalc";
+            this.Load += new System.EventHandler(this.AgeCalc_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labRes;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp9/AgeCalc.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class AgeCalc : Form
+    {
+        public AgeCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void AgeCalc_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        DateTime SelectedPicker;
+        ComboBox CurrentOption;
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+            SelectedPicker = dateTimePicker1.Value;
+            UpdateLabel(Sitem: CurrentOption.SelectedItem.ToString()); // to update after changing
+         
+        }
+
+
+        void UpdateLabel(string Sitem)
+        {
+            DateTime dt = DateTime.Now;
+            TimeSpan sp = dt - (SelectedPicker); // the diff between now and per date 
+
+            switch (Sitem)
+            {
+                case "Get Your Age in Days":
+                    labRes.Text = "Your Age In Days: " + sp.Days;
+
+                    break;
+
+                case "Get Your Age in Hours":
+                    labRes.Text = "Your Age In Hours: " + sp.TotalHours;
+
+                    break;
+
+                case "Get Your Age in Months":
+                    labRes.Text = "Your Age In Months: " + (sp.Days / 30);
+
+                    break;
+
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+            ComboBox Sitem = sender as ComboBox;
+            CurrentOption = Sitem;
+            UpdateLabel(Sitem:Sitem.SelectedItem.ToString());
+
+
+        }
+
+    }
+}
