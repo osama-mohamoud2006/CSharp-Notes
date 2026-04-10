57c7face7781e417a885b3d9a16e07d1c440653b
osama mohamoud <wwwhdosama@gmail.com>
2026-04-10 14:33:16 +0200

    Auto commit: Fri Apr 10 14:33:16 EST 2026

--- 14 - C# - Level 1/Windows Forms/WindowsFormsApp9/Form1.cs
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp9/Form1.cs
@@ -36,6 +36,42 @@ namespace WindowsFormsApp9
 
         private void textBox1_Click(object sender, EventArgs e)
         {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.RemoveAt(0); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show((checkedListBox1.Items.Count - checkedListBox1.CheckedItems.Count).ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //foreach (var item in checkedListBox1.Items) // get the checked items 
+            //{
+            //    if(checkedListBox1.CheckedItems.Contains(item)) MessageBox.Show(item.ToString() + " Has checked");
+            //}
+            if (checkedListBox1.CheckedItems.Count == 0) return;
+
+            for (var i = 1; i <= checkedListBox1.CheckedItems.Count; i++)
+            {
+                MessageBox.Show(checkedListBox1.CheckedItems[i - 1].ToString());
+            }
+            MessageBox.Show(checkedListBox1.Items[0].ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count == 0) return; // no thing to be unchecked
+
+            for (var i = 1; i <= checkedListBox1.Items.Count; i++) // the all  items 
+            {
+                checkedListBox1.SetItemChecked(i-1, false); // false means uncheck it 
+            }
 
         }
     }
