270869953d2274e709a4c7588a8d259ef539a764
osama mohamoud <wwwhdosama@gmail.com>
2026-04-11 00:40:20 +0200

    Auto commit: Sat Apr 11 00:40:18 EST 2026

---
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp9/Form3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("سلام يبن العبيطه", "Bye!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            Process.Start("shutdown", "/s /t 3");
+        }
+    }
+}
--- 14 - C# - Level 1/Windows Forms/WindowsFormsApp9/Program.cs
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp9/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp9
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MonthCalnder());
+            Application.Run(new Form3());
         }
     }
 }
