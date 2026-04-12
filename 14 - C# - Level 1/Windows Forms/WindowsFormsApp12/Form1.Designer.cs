b1e8d71d680ec2f68213b1c4419330ac6080b47c
osama mohamoud <wwwhdosama@gmail.com>
2026-04-12 21:29:30 +0200

    Auto commit: Sun Apr 12 21:29:28 EST 2026

---
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp12/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Child1");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Child2");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Child3");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Child11");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Node1", new System.Windows.Forms.TreeNode[] {
+            treeNode5});
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Node2");
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "energetic.png");
+            this.imageList1.Images.SetKeyName(1, "gojo satoru.jpg");
+            this.imageList1.Images.SetKeyName(2, "cat.jpg");
+            this.imageList1.Images.SetKeyName(3, "icons8-c-sharp-logo-100.png");
+            // 
+            // treeView1
+            // 
+            this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.ImageIndex = 2;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Indent = 100;
+            this.treeView1.ItemHeight = 50;
+            this.treeView1.Location = new System.Drawing.Point(23, 34);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Child1";
+            treeNode1.Text = "Child1";
+            treeNode2.Name = "Node7";
+            treeNode2.Text = "Child2";
+            treeNode3.Name = "Child3";
+            treeNode3.Text = "Child3";
+            treeNode4.Name = "Node0";
+            treeNode4.Text = "Node0";
+            treeNode5.Name = "Child11";
+            treeNode5.Text = "Child11";
+            treeNode6.Name = "Node1";
+            treeNode6.Text = "Node1";
+            treeNode7.Name = "Node2";
+            treeNode7.Text = "Node2";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode6,
+            treeNode7});
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(703, 362);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.Click += new System.EventHandler(this.treeView1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(563, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 92);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(563, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 92);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(563, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(225, 92);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Add Child";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(292, 295);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(225, 92);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "What Has Checked";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 502);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp12/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static byte conter =0;  
+
+        struct stdata
+        {
+            public string name;
+            public int age;
+
+            public override string ToString()
+            {
+                return $"{name},{age}";
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stdata d;
+            d.name = "Ahmed";
+            d.age = 20;
+                
+            treeView1.Nodes.Add(d.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(!(treeView1.Nodes.Count<1) )
+            treeView1.Nodes.RemoveAt(treeView1.Nodes.Count-1 );
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            treeView1.SelectedNode.Nodes.Add("Child " + conter++) ;
+        }
+
+        private void treeView1_Click(object sender, EventArgs e) // Node 
+        {
+            this.BackColor = Color.AliceBlue;
+            //  MessageBox.Show("Node Clicked") ;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e) // Child 
+        {
+            if(treeView1.SelectedNode.Nodes!=null) // The Child Of The Current Selected Node
+                MessageBox.Show(treeView1.SelectedNode.Nodes.Text );
+        }
+    }
+}
