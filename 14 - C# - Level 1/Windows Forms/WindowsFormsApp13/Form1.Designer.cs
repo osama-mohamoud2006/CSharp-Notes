1464aadd530f147e42ed0252e1f534226d5d340e
osama mohamoud <wwwhdosama@gmail.com>
2026-04-13 21:09:26 +0200

    Auto commit: Mon Apr 13 21:09:25 EST 2026

--- 14 - C# - Level 1/Windows Forms/WindowsFormsApp13/Form1.Designer.cs
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp13/Form1.Designer.cs
@@ -29,12 +29,105 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.progressBar1.ForeColor = System.Drawing.Color.Transparent;
+            this.progressBar1.Location = new System.Drawing.Point(51, 174);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(673, 81);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Set value to 0";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(228, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 56);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Set value to 50";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(400, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 56);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Set value to 100";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(590, 315);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 56);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Add 10 To Progress";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(239, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 93);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Progress";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- 14 - C# - Level 1/Windows Forms/WindowsFormsApp13/Form1.cs
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp13/Form1.cs
@@ -5,16 +5,72 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp13
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            progressBar1.Minimum = 0; 
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            progressBar1.BackColor = Color.Violet; 
+            MessageBox.Show("Clicked");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            label1.Text = (((float)progressBar1.Value / progressBar1.Maximum) * 100).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 50;
+            label1.Text = (((float)progressBar1.Value / progressBar1.Maximum) * 100).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 100;
+            label1.Text = (((float)progressBar1.Value / progressBar1.Maximum) * 100).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            progressBar1.Minimum = 0;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            for (int i = 1; i<= 10; i++ )
+            {
+                if (progressBar1.Value < progressBar1.Maximum)
+                {
+                   // Thread.Sleep(500);
+                    progressBar1.Value += 10;
+                    
+                    label1.Text = ((((float)progressBar1.Value / progressBar1.Maximum) * 100).ToString()) + "%";
+                    progressBar1.Refresh();
+                    label1.Refresh();
+
+                   
+                }
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
