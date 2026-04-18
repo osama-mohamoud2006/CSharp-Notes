f7973645b45389ac86e67b530c368d728288fa7d
osama mohamoud <wwwhdosama@gmail.com>
2026-04-18 22:58:20 +0200

    Auto commit: Sat Apr 18 22:58:18 EST 2026

---
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp15/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Login = new System.Windows.Forms.TabPage();
+            this.Manage = new System.Windows.Forms.TabPage();
+            this.List = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabControl1.Controls.Add(this.Login);
+            this.tabControl1.Controls.Add(this.Manage);
+            this.tabControl1.Controls.Add(this.List);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Login
+            // 
+            this.Login.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login.Location = new System.Drawing.Point(29, 4);
+            this.Login.Name = "Login";
+            this.Login.Padding = new System.Windows.Forms.Padding(3);
+            this.Login.Size = new System.Drawing.Size(767, 442);
+            this.Login.TabIndex = 4;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            // 
+            // Manage
+            // 
+            this.Manage.Location = new System.Drawing.Point(29, 4);
+            this.Manage.Name = "Manage";
+            this.Manage.Size = new System.Drawing.Size(767, 442);
+            this.Manage.TabIndex = 5;
+            this.Manage.Text = "Manage";
+            this.Manage.UseVisualStyleBackColor = true;
+            // 
+            // List
+            // 
+            this.List.Location = new System.Drawing.Point(29, 4);
+            this.List.Name = "List";
+            this.List.Size = new System.Drawing.Size(767, 442);
+            this.List.TabIndex = 6;
+            this.List.Text = "List";
+            this.List.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Login;
+        private System.Windows.Forms.TabPage Manage;
+        private System.Windows.Forms.TabPage List;
+    }
+}
+
