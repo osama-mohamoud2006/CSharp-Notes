45b4603dc6dcc305f10ea9beba0302dbee904421
osama mohamoud <wwwhdosama@gmail.com>
2026-04-23 00:27:20 +0200

    Auto commit: Thu Apr 23 00:27:19 EST 2026

--- 14 - C# - Level 1/Windows Forms/WindowsFormsApp15/Form6.cs
+++ 14 - C# - Level 1/Windows Forms/WindowsFormsApp15/Form6.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApp15
 {
@@ -20,14 +21,17 @@ namespace WindowsFormsApp15
         private void button1_Click(object sender, EventArgs e)
         {
             saveFileDialog1.InitialDirectory = @"E:\"; // to make the file save dialog starts with specific dir 
-            saveFileDialog1.DefaultExt = "png"; // to set the default file extension 
+            saveFileDialog1.DefaultExt = "text"; // to set the default file extension 
             saveFileDialog1.Title = "Hello World"; // to set the title of file save dialog 
 
-            saveFileDialog1.Filter = "text(*.text)|*.text|png(*.png)|*.png"; // to set specific file filtering according to its type (text|*.fileExt)
+            saveFileDialog1.Filter = "text(*.text)|*.text|png(*.png)|*.png|Sound(*.mp3)|*.mp3|All Files(*.*)|*.*"; // to set specific file filtering according to its type (text|*.fileExt)
+
+            saveFileDialog1.FilterIndex = 100; // to select the default filter (it starts from 1 not 0 and if you have set index larger than actual filter you have it will back to the last available index)
 
             if (saveFileDialog1.ShowDialog()==DialogResult.OK)
             {
                 MessageBox.Show(saveFileDialog1.FileName);
+                File.WriteAllText(saveFileDialog1.FileName, "Hello World!");
             }
         }
     }
